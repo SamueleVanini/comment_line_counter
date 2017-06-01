363b8d2313e17dab3e524b07d4c21da79730a17a
SamueleVanini <samuele.vanini@live.it>
2017-06-01 17:50:03 +0200

    Controllo righe totali corretto

--- Main.py
+++ Main.py
@@ -7,10 +7,11 @@ for line in file_input:
     if line.startswith("'''") and comment_block is False:
         comment_count += 1
         comment_block = True
+    elif line[-1] is "'" and line[-2] is "'" and line[-3] is "'" and comment_block is True:
+        comment_block = False
+        comment_count += 1
     elif comment_block is True:
         comment_count += 1
     elif line.startswith('#'):
         comment_count += 1
-    elif line[-1] is "'" and line[-2] is "'" and line[-3] is "'" and comment_block is True:
-        comment_block = False
 print(comment_count)
