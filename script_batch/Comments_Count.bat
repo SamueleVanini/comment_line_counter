bff6e8bc4f77f4a5a570b37b8baa20f8c633d5d0
SamueleVanini <samuele.vanini@live.it>
2017-06-09 09:20:16 +0200

    Aggiunta eliminazione file a dopo l'ultimo controllo dell'ultimo file

--- script_batch/Comments_Count.bat
+++ script_batch/Comments_Count.bat
@@ -5,6 +5,7 @@ set @filepy=Comments_Count.py
 set @path=%@path:\script_batch=%
 set @path=%@path%%@filepy%
 python %@path% %1 > Output
-SET /p MYVAR=<Output
-ECHO %MYVAR%
-DEL Output
+SET /p Perc_Comm=<Output
+ECHO %Perc_Comm%
+ECHO. >Output
+if %2 EQU DEL DEL Output
